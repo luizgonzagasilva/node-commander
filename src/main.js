38adc9ff1f285e5d0e55e9e8780ab54b613a6e0f
luizgonzaga <desconhecido.conhecido@hotmail.com>
2020-06-02 12:38:03 -0300

    0.0.1 node-commander

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import { getCityId, getCityForecastById } from './utils/requests'
+
+export default async function getForecast(cityName){
+
+    try{
+
+        const cityId = await getCityId(cityName)
+        const cityForecast = await getCityForecastById(cityId)
+
+        console.log(cityId)
+
+        console.log(`
+        Clima em ${cityForecast.name}, ${cityForecast.state}
+            Temperatura: ${cityForecast.temperature}°
+            Direção do vento: ${cityForecast.wind_direction}
+            Velocidade do vento: ${cityForecast.wind_velocity} km/h
+            Humidade: ${cityForecast.humidity}
+            Condição: ${cityForecast.condition}
+            Pressão atmosférica: ${cityForecast.pressure}
+            Sensação: ${cityForecast.sensation}°
+        `)
+
+    } catch (error){
+        console.log(error.message)
+    }
+
+}
